a5f85ed0e94697553d6520a3ee7fdb553842b206
Paulo Prado <166643153+Paulo-1986@users.noreply.github.com>
2024-08-07 10:07:03 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+let leitura = 0
+basic.showIcon(IconNames.Heart)
+basic.pause(2000)
+basic.clearScreen()
+basic.pause(200)
+basic.forever(function () {
+    leitura = pins.analogReadPin(AnalogPin.P0)
+    if (leitura < 500) {
+        basic.showIcon(IconNames.No)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(2000)
+        basic.showNumber(leitura)
+        basic.pause(2000)
+    } else {
+        basic.showIcon(IconNames.Yes)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(2000)
+        basic.showNumber(leitura)
+        basic.pause(2000)
+    }
+})
